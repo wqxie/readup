e44ceafadd06d7fe869aa4bf75e75345e13e43c9
wqxie <wqxie@umich.edu>
2014-05-11 02:26:11 -0400

    Finish the reading page. Modify club page.

--- js/iphone.js
+++ js/iphone.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+	
 	$('.tab-lib').click(function(){
 		$('.tab-selected').removeClass('tab-selected');
 		$(this).addClass('tab-selected');
@@ -37,6 +38,8 @@ $(document).ready(function(){
 		$('.content-page').hide();
 		$('#page-lib').show();
 		$('.header .title').text("Library");
+		$('#page-read').animate({left:'0px'});
+		$('#page-read').animate({top:'570px'});
 	});
 	$('#menu-profile').click(function(){
 		$('.iphone-container').animate({left:'0px'});
@@ -47,6 +50,8 @@ $(document).ready(function(){
 		$('.content-page').hide();
 		$('#page-profile').show();
 		$('.header .title').text("Profile");
+		$('#page-read').animate({left:'0px'});
+		$('#page-read').animate({top:'570px'});
 	});
 	$('.book').click(function(){
 		$('.tab-selected').removeClass('tab-selected');
@@ -54,19 +59,102 @@ $(document).ready(function(){
 		$('#page-book').show();
 		$('.header .title').text("Book");
 	});
+	$('.club-rect').click(function(){
+		$('.content-page').hide();
+		$('#page-clubinfo').show();
+		$('.header .title').text("BookClub");
+	});
+	$('.club-name').click(function(){
+		$('.content-page').hide();
+		$('#page-clubinfo').show();
+		$('.header .title').text("BookClub");
+	});
 
+	$('.start-book').click(function(){
+  		$('.content-page').hide();
+		$('.header .title').text('Reading...');
+		$('#page-read').animate({top:'65px'});
+		var delay=0;
+		$('.start-booklist .start-book').each(function(){
+			$(this).delay(delay).animate({top:'600px'},1000);
+			delay+=200;
+		});			
+		$('.blur-bg').hide();
+		$('.iphone-container').removeClass('blur');
+		$('.add-btn').hide();
+		$('.exit-btn').show();
+	});
 
+	$('.exit-btn').click(function(){
+		$('.exit-btn').hide();
+		$('.add-btn').show();
+		$('#page-lib').show();
+		$('.header .title').text('Library');
+		$('#page-read').animate({top:'570px'});
+		$('.tab-selected').removeClass('tab-selected');
+		$('.tab-lib').addClass('tab-selected');
+	});
 
+	$('.reading-footer-tab .circle-tab').click(function(){
+		if($('#play-btn').hasClass('clicked'))
+		{
+			$('.reading-footer-tab .circle-tab img').attr("src", "./images/pause.png");
+			$('.reading-footer-tab .circle-tab p').html("Stop");
+			$('.header .title').text('Reading...');
+			
+			$('#play-btn').removeClass('clicked');
+		}else{
+			$('.reading-footer-tab .circle-tab img').attr("src", "./images/play.png");
+			$('.reading-footer-tab .circle-tab p').html("Continue");
+			$('.header .title').text('Pause');
+			$('#play-btn').addClass('clicked');
+		}
+	});
+
+	$('.tab-happy').click(function(){
+		$('.emotion img').attr("src","./images/happy.png");
+		$('.emotion p').text('Happy');
+		$('.blur-bg-read').show();
+		$('.message-box').show();
+	});
+	$('.tab-sad').click(function(){
+		$('.emotion img').attr("src","./images/sad.png");
+		$('.emotion p').text('Sad');
+		$('.blur-bg-read').show();
+		$('.message-box').show();
+	});
+	$('.tab-cry').click(function(){
+		$('.emotion img').attr("src","./images/cry.png");
+		$('.emotion p').text('Cry');
+		$('.blur-bg-read').show();
+		$('.message-box').show();
+	});
+	$('.tab-angry').click(function(){
+		$('.emotion img').attr("src","./images/angry.png");
+		$('.emotion p').text('Angry');
+		$('.blur-bg-read').show();
+		$('.message-box').show();
+	});
+	$('.message-box .submit-btn').click(function(){
+		$('.blur-bg-read').hide();
+		$('.message-box').hide();
+	});
+	$('.message-box .cancel-btn').click(function(){
+		$('.blur-bg-read').hide();
+		$('.message-box').hide();
+	});
 
 	$('.list-btn').click(function(){
 		if ($('.iphone-container').css("left")=='0px') {
 			$('.iphone-container').animate({left:'200px'});
 			$('.footer-tab').animate({left:'200px'});
 			$('.hidden-menu').animate({left:'0px'});
+			$('#page-read').animate({left:'200px'});
 		}else{
 			$('.iphone-container').animate({left:'0px'});
 			$('.footer-tab').animate({left:'0px'});
 			$('.hidden-menu').animate({left:'-200px'});
+			$('#page-read').animate({left:'0px'});
 		}
 	});
 	$('.add-btn').click(function(){
@@ -75,11 +163,11 @@ $(document).ready(function(){
 		$('.header .title').text("Add Book");
 	});
 
-	$('.circle-tab').click(function(){
-		if($('.circle-tab p').text() == 'Read')
+	$('#footer-panel .circle-tab').click(function(){
+		if($('#footer-panel .circle-tab p').text() == 'Read')
 		{
-			$('.circle-tab img').attr("src", "./images/social.png");
-			$('.circle-tab p').html("Select");
+			$('#footer-panel.circle-tab img').attr("src", "./images/social.png");
+			$('#footer-panel .circle-tab p').html("Select");
 			var delay=0;
 			$('.start-booklist .start-book').each(function(){
 				$(this).delay(delay).animate({top:'400px'},1000);
@@ -88,8 +176,8 @@ $(document).ready(function(){
 			$('.blur-bg').show();
 			$('.iphone-container').addClass('blur');
 		}else{
-			$('.circle-tab img').attr("src", "./images/read.png");
-			$('.circle-tab p').html("Read");
+			$('#footer-panel .circle-tab img').attr("src", "./images/read.png");
+			$('#footer-panel .circle-tab p').html("Read");
 			var delay=0;
 			$('.start-booklist .start-book').each(function(){
 				$(this).delay(delay).animate({top:'600px'},1000);
@@ -148,24 +236,52 @@ $(document).ready(function(){
 		$('#suggestion').hide();
 		$('#wishlist').show();
 	});
+	$('.btn-recommend').click(function(){
+		$('.recommend-box').show();
+	});
+	$('.recommend-box .submit-btn').click(function(){
+		$('.recommend-box').hide();
+	});
+	$('.recommend-box .cancel-btn').click(function(){
+		$('.recommend-box').hide();
+	});
+	$('.btn-scan').click(function(){
+		$('.page-addbook').hide();
+		$('.tab-selected').removeClass('tab-selected');
+		$('tab-lib').addClass('tab-selected');
+		$('.content-page').hide();
+		$('#page-lib').show();
+		$('.header .title').text("Library");
+	});
 
-	var quotes=["Let me think what to eat? Fiction? Novel?",
+	var quotes=["Hey!!! Click me. I can speak!",
+	"Hire me. I am the most talented UX worm in the world! :)",
+	"Let me think what to eat? Fiction? Novel?",
 	"To eat or not to eat, that is not a question",
 	"Hey! Read more books so that I have something to eat.",
-	"Yuqing is the most beatiful woman:)"];
+	"Hi, pretty, can you give ISBN of the book you are reading?"];
 	$('.pic-worm').click(function(){
-		$(this).find('.quote-worm').text(quotes[Math.floor(Math.random()*4)]);
+		$(this).find('.quote-worm').text(quotes[Math.floor(Math.random()*6)]);
 	});
 
 	var pageno=54;
+	var status=0;
 	$('.btn-feed').click(function(){
-		$('.info .status').removeClass('red');
-		$('.info .status').addClass('green');
-		$('.info .status').text('Full');
 		$('.add-food p').text('Food: ' + pageno + ' pages left.');
 		$('.food').animate({right:'185px',top:'-60px'},500,function(){$('.food').css("opacity",0).css("right","48px").css("top","16px").css("opacity",1);});
+		if(status<4){
+			status++;
+		}
+		$('#status-no').text(status);
+		if (status == 4) {
+			$('.info .status').removeClass('red');
+			$('.info .status').addClass('green');
+			$('.info .status').text('Full');
+		};
 		pageno--;
 	});
 
 
+
 })//function ready end
+
